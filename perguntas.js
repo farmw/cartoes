65028f4fc2520f4a88756843323e747c0beffaf9
Paulo Henrique <paulomenezesm6@gmail.com>
2024-11-29 19:16:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+   'física',
+   'O que acontece com a luz ao passar por um prisma?',
+    'A luz é refratada e separada em várias cores, formando um espectro, porque diferentes comprimentos de onda se desviam em ângulos diferentes'
+)
+
+criaCartao(
+    'física',
+    'Qual é a diferença entre calor e temperatura em um sistema termodinâmico?',
+    'Calor é a energia transferida entre dois corpos devido à diferença de temperatura, enquanto temperatura é a medida da energia cinética média das partículas de um corpo'
+)
+
+criaCartao(
+    'física',
+    'O que acontece com a imagem formada por um espelho plano?',
+    ' A imagem formada por um espelho plano é virtual, direita e de mesma altura do objeto. Ela é formada atrás do espelho e é simétrica em relação ao espelho'
+)
+criaCartao(
+    'física',
+    'O que ocorre com a luz quando ela passa de um meio menos denso para um meio mais denso?',
+   'A luz sofre refração, ou seja, ela se desvia em direção à normal, e sua velocidade diminui ao entrar no meio mais denso'
+)
+
+criaCartao(
+   'física',
+    'O que é entropia e como ela se relaciona com a irreversibilidade dos processos naturais?',
+    'A entropia é uma medida da desordem ou aleatoriedade de um sistema. Em processos naturais, a entropia tende a aumentar, o que reflete a tendência dos sistemas de evoluírem para estados mais desordenados e irreversíveis'
+)
+
+criaCartao(
+   'física',
+    ' O que é a amplitude de uma onda e como ela afeta a energia transportada pela onda?',
+    'A amplitude de uma onda é a altura máxima da oscilação da partícula em relação à posição de equilíbrio. Quanto maior a amplitude, maior a energia transportada pela onda, pois a energia é proporcional ao quadrado da amplitude'
+)
